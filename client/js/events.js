b17c46db25543db95d6b8c7a1620bfcefa0c1f8b
allenwhitedev <allenwhitedev@gmail.com>
2015-11-01 15:35:36 -0500

    Init commit, setup basic file layout, add materialize, sideNav, navbar, and router with default layout

---
+++ client/js/events.js
@@ -0,0 +1,5 @@
+Template.navbar.events
+({
+	'click .nav-wrapper > ul > li': function(){ $('.button-collapse').sideNav('hide') }
+})
+
---
+++ client/router.js
@@ -0,0 +1,9 @@
+Router.configure({ layoutTemplate: 'defLayout' })
+
+Router.map(function()
+{
+	this.route('home', {path: '/'})
+	this.route('userAccounts')
+	this.route('navbar')
+})
+
